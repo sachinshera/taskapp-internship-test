22e9c19d17ff19499223b982ec68fe7590657b3e
= <=>
2022-10-08 18:15:22 +0530

    mongo db atlas added

--- db.js
+++ db.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 
 // connect app database
 
+let dbUrl =  "mongodb+srv://sachin:sachin@cluster0.9x3ucir.mongodb.net/?retryWrites=true&w=majority";
+
 const connect = ()=>{
-    mongoose.connect('mongodb://localhost:27017/taskApp', {useNewUrlParser: true, useUnifiedTopology: true}, (err)=>{
+    mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true}, (err)=>{
         if(err){
             console.log(err);
         }else{
